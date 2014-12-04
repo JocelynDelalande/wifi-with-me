249f9752dd14806429cfe4c0c94479887d3f4235
opi <opi@no-log.org>
2014-12-04 20:53:44 +0100

    Disable wheel scroll on map.

--- assets/main.js
+++ assets/main.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
     }
 
     // Create map
-    var map = L.map('map').setView([defaults.lat,defaults.lng], defaults.zoom);
+    var map = L.map('map', {scrollWheelZoom: false}).setView([defaults.lat,defaults.lng], defaults.zoom);
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18
