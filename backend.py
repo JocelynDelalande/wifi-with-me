JocelynDelalande/wifi-with-me

commit 842c3b9300b43f6318db3a20811433d080310828

validate floor numbers are positive fixes #10

backend.py
@@ -159,6 +159,10 @@ def submit_wifi_form():
         errors.append((field_names['floor'], "ce champ est requis"))
     elif floor and floor_total and (int(floor) > int(floor_total)):
         errors.append((field_names['floor'], "Étage supérieur au nombre total"))
+    if (int(floor) < 0):
+         errors.append((field_names['floor'], "l'étage doit-être positif"))
+    if (int(floor_total) < 0):
+         errors.append((field_names['floor_total'], "le nombre d'étages doit-être positif"))
 
     if errors:
         return template('wifi-form', errors=errors, data=request.forms,

commit 7ea23ebbbca8a42123dc2a0f5f55f86a5ce88660

Fix floor validation when no floor is provided (bug introduced in 842c3b9)

backend.py
@@ -159,10 +159,10 @@ def submit_wifi_form():
         errors.append((field_names['floor'], "ce champ est requis"))
     elif floor and floor_total and (int(floor) > int(floor_total)):
         errors.append((field_names['floor'], "Étage supérieur au nombre total"))
-    if (int(floor) < 0):
-         errors.append((field_names['floor'], "l'étage doit-être positif"))
-    if (int(floor_total) < 0):
-         errors.append((field_names['floor_total'], "le nombre d'étages doit-être positif"))
+    elif floor and (int(floor) < 0):
+        errors.append((field_names['floor'], "l'étage doit-être positif"))
+    elif floor_total and (int(floor_total) < 0):
+        errors.append((field_names['floor_total'], "le nombre d'étages doit-être positif"))
 
     if errors:
         return template('wifi-form', errors=errors, data=request.forms,