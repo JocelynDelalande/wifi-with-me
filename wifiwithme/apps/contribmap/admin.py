2bd637e2e8697489e883c765527ce29468a63c90
Jocelyn Delande <jocelyn@crapouillou.net>
2016-03-03 15:43:53 +0100

    Enhance data hierarchy in cotnrib form

--- wifiwithme/apps/contribmap/admin.py
+++ wifiwithme/apps/contribmap/admin.py
@@ -8,3 +8,30 @@ from .models import Contrib
 class ContribAdmin(admin.ModelAdmin):
     search_fields = ["name", "email", "phone"]
     list_display = ("name", "date",)
+
+    fieldsets = [
+        [None, {
+            'fields': ['name', 'contrib_type'],
+        }],
+        ['Localisation', {
+            'fields': [
+                ('latitude', 'longitude'),
+                ('floor', 'floor_total'),
+                'orientations', 'roof']
+        }],
+        ['Raccordement au réseau', {
+            'fields': ['connect_local', 'connect_internet'],
+            'classes': ['collapse'],
+        }],
+        ['Partage de connexion', {
+            'fields': ['access_type', 'bandwidth', 'share_part'],
+            'classes': ['collapse'],
+        }],
+        ['Vie privée', {
+            'fields': [
+                'privacy_name', 'privacy_email', 'privacy_coordinates',
+                'privacy_place_details', 'privacy_comment'
+            ],
+            'classes': ['collapse'],
+        }]
+    ]
