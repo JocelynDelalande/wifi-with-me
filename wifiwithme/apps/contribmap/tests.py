1ac6ee5d02f65209361ad369f46ab20116c399aa
Jocelyn Delande <jocelyn@crapouillou.net>
2016-03-03 12:31:15 +0100

    Fix existing data for Django use
    Weird issue with empty FloatField

--- wifiwithme/apps/contribmap/tests.py
+++ wifiwithme/apps/contribmap/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from contribmap.models import Contrib
+
+
+class TestDataImport(TestCase):
+    fixtures = ['bottle_data.yaml']
+
+    def test_re_save(self):
+        for contrib in Contrib.objects.all():
+            contrib.full_clean()
+            contrib.save()
