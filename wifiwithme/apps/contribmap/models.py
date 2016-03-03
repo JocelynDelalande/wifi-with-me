e5012b3d3a87785ef96ee0438b11258bcd8e55c3
Jocelyn Delande <jocelyn@crapouillou.net>
2016-03-03 12:27:33 +0100

    Let django manage the bottle model

--- wifiwithme/apps/contribmap/models.py
+++ wifiwithme/apps/contribmap/models.py
@@ -1,17 +1,10 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
 
 
 class Contrib(models.Model):
-    id = models.AutoField(primary_key=True, blank=True, null=True)
+    id = models.AutoField(primary_key=True, blank=False, null=False)
     name = models.TextField(blank=True, null=True)
     contrib_type = models.TextField(blank=True, null=True)
     latitude = models.FloatField(blank=True, null=True)
@@ -36,5 +29,5 @@ class Contrib(models.Model):
     date = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'contribs'
